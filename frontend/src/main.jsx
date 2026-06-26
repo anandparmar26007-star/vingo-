083803e4185caf470a92053b983716d706ea0d3f
anand001git <anandparmar20007@gmail.com>
2026-06-26 11:30:41 +0530

    Update: Vite project me changes kiye

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Nav from "./pages/Home";
 
 
 import "./index.css"; 
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
       <BrowserRouter>
 
-        <App/>
+        <Nav/>
         
 
       </BrowserRouter>
